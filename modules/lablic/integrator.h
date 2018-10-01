cb41208d10e0d01916bd5796f02c8e678f6659f6
vale <bolinto@gmail.com>
2018-10-01 12:13:37 +0200

    Integrator copied to new lab

--- modules/lablic/integrator.h
+++ modules/lablic/integrator.h
@@ -13,6 +13,7 @@
 #include <lablic/lablicmoduledefine.h>
 #include <inviwo/core/common/inviwo.h>
 #include <inviwo/core/datastructures/geometry/basicmesh.h>
+#include <inviwo/core/datastructures/volume/volume.h>
 #include <functional>
 
 namespace inviwo {
@@ -29,12 +30,23 @@ public:
 // Methods
 public:
 
-    // TODO: Build on the last assignment by copying the integrator code
-    // here and in the respective .cpp. Mark a small change in the vector field sampling
-    // (change the parameter VolumeRAM* to Volume*, i.e., vr to vol.get()).
-    // You may want to consider adding a helper function that computes an entire streamline
-    // if you have not done so for the last assignment already.
+    static vec2 sampleFromField(const Volume* vol, size3_t dims, const vec2& position, bool isNormalized);
 
+    static vec2 rk4(const Volume* vol, size3_t dims, const vec2& position, float stepSize);
+    
+    //input position: 16x16 -> 
+    //output:         16x16, stepsize added
+    static vec2 euler(const Volume* vol, size3_t dims, const vec2& position, float stepSize);
+    
+    
+    //input v1,v2: 16x16 ->
+    //draws line based on dimensions
+    static void drawLineSegmentAndPoints(const vec2& v1, const vec2& v2,
+                                         size3_t dims,
+                                         IndexBufferRAM* indexBufferLines,
+                                         IndexBufferRAM* indexBufferPoints,
+                                         std::vector<BasicMesh::Vertex>& vertices,
+                                         const vec4& color);
 };
 
 }  // namespace inviwo
